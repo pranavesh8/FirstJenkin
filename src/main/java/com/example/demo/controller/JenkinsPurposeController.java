084d0c2a2a351274a722c6080ac977a866424f5f
pranavesh8 <105205641+pranavesh8@users.noreply.github.com>
2022-12-09 14:52:25 +0530

    Update JenkinsPurposeController.java

--- src/main/java/com/example/demo/controller/JenkinsPurposeController.java
+++ src/main/java/com/example/demo/controller/JenkinsPurposeController.java
@@ -1,13 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JenkinsPurposeController {
 	
-	@GetMapping("/welcome")
+	@RequestMapping("/welcome")
 	public String method() {
 		return "Hai Pranavesh";
 		
