8e9a06335456151004d28edc08ca64d0f03bfe3d
pranavesh8 <105205641+pranavesh8@users.noreply.github.com>
2022-12-12 12:44:37 +0530

    Update JenkinPurposeApplicationTests.java

--- src/test/java/com/example/demo/JenkinPurposeApplicationTests.java
+++ src/test/java/com/example/demo/JenkinPurposeApplicationTests.java
@@ -3,7 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest(classes={com.example.demo.JenkinPurposeApplication.class})
+@SpringBootTest
+//(classes={com.example.demo.JenkinPurposeApplication.class})
 class JenkinPurposeApplicationTests {
 
 	@Test
