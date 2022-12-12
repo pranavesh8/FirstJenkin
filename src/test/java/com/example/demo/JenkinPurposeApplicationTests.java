1bb48edb934744db96f675bdaaf7eb2e4541eb63
pranavesh8 <105205641+pranavesh8@users.noreply.github.com>
2022-12-12 12:43:30 +0530

    Update JenkinPurposeApplicationTests.java

--- src/test/java/com/example/demo/JenkinPurposeApplicationTests.java
+++ src/test/java/com/example/demo/JenkinPurposeApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes={com.example.demo.JenkinPurposeApplication.class})
+//@SpringBootTest(classes={com.example.demo.JenkinPurposeApplication.class})
 class JenkinPurposeApplicationTests {
 
 	@Test
